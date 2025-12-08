9d6b6041e2f5f665ed93ab73979ee09e1f107e02
texnar1511 <texnar1511@gmail.com>
2025-12-08 12:37:16 +0300

    queue moving

--- keyboard_lsl.py
+++ keyboard_lsl.py
@@ -16,14 +16,32 @@ screen = pygame.display.set_mode(size = (0, 0), flags = flags)
 width  = screen.get_width()
 height = screen.get_height()
 
-n_cols = 11
-n_rows = 4
+delay = 1
+
+n_cols = 5
+n_rows = 9
+
+alphabet = '''
+1234567890_
+ЙЦУКЕНГШЩЗХ
+ФЫВАПРОЛДЖЭ
+ЁЯЧСМИТЬБЮЪ
+'''.replace('\n', '')
 
 w, h = width // n_cols, height // n_rows
 
 sec_in_msec = 1e-3
 
-speed_func = lambda t, freq: math.sin(t * freq * math.pi * sec_in_msec / 2) ** 2
+#speed_func = lambda t, freq: math.sin(t * freq * math.tau * sec_in_msec)
+
+def speed_func(t, freq = 1, t1 = 0.5, t2 = 0.5):
+    if t1 / sec_in_msec <= t <= (t1 + 1 / freq) / sec_in_msec:
+        return math.sin((t - t1 / sec_in_msec) * math.tau * freq * sec_in_msec)
+    elif 0 <= t < t1 / sec_in_msec or (t1 + 1 / freq) / sec_in_msec < t <= (t1 + 1 / freq + t2) / sec_in_msec:
+        return 0
+    else:
+        raise ValueError(f"t must be between 0 and {(t1 + 1 / freq + t2) / sec_in_msec}")
+    #return math.sin((t - t1) * math.tau * freq * sec_in_msec) if t1 / sec_in_msec <= t <= (t1 + 1 / freq) / sec_in_msec else 0 if 0 <= t < t1 / sec_in_msec or (t1 + 1 / freq) / sec_in_msec < t < (t1 + 1 / freq + t2) / sec_in_msec else ValueError(f"t must be between 0 and {(t1 + 1 / freq + t2) / sec_in_msec}")
 
 info = StreamInfo(name = 'annotations', type = 'Events', channel_count = 1, nominal_srate = 0, channel_format = 'string', source_id = 'my_marker_stream')
 
@@ -35,30 +53,26 @@ print('lsl outlet is created')
 
 pygame.display.set_caption('Experiment')
 clock = pygame.time.Clock()
-t0 = pygame.time.get_ticks()
 
 background = (255, 255, 255)
-letter_foreground = (255, 255, 255)
-shadow_foreground = (0, 0, 0)
+letter_foreground = (0, 0, 0)
 
 FPS = 60
 
-font = pygame.font.SysFont(name = 'Arial', size = 50, bold = True)
-
-alphabet = '1234567890_ЙЦУКЕНГГЩЗХФЫВАПРОЛДЖЭЁЯЧСМИТЬБЮЪ'
+font = pygame.font.SysFont(name = 'Arial', size = 30, bold = True)
 
 cells = {}
+
+#print(w)
 
 for id, char in enumerate(alphabet):
     cells[char] = {
         'id': id,
         'letter': font.render(char, True, letter_foreground),
-        'shadow': font.render(char, True, shadow_foreground),
-        'alpha': random.randint(0, 1) / 2, #random.random(),
-        'amplitude': 40, #random.random() * 15 + 5,
-        'frequency': 1, #random.random() * 4.8 + 0.2,
-        'previous_speed': 0,
-        'decrease': False
+        #'alpha': random.randint(0, 1) / 2, #random.random(),
+        'amplitude': (w / 2) * 0.9, #random.random() * 15 + 5,
+        'frequency': 2, #random.random() * 4.8 + 0.2,
+        #'moving': True,
     }
     
 #print(cells)
@@ -66,6 +80,13 @@ for id, char in enumerate(alphabet):
 running = True
 
 start_experiment = False
+
+moving_id = random.choice(range(len(alphabet)))
+
+#print(moving_id)
+
+t0 = pygame.time.get_ticks()
+start_t = 0
 
 while running:
     
@@ -78,13 +99,13 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
                 
-            if event.key == pygame.K_s:
-                start_experiment = True
-                outlet.push_sample(['start_experiment'])
-            
-            if event.key == pygame.K_e:
-                start_experiment = False
-                outlet.push_sample(['end_experiment'])
+            #if event.key == pygame.K_s:
+            #    start_experiment = True
+            #    outlet.push_sample(['start_experiment'])
+            #
+            #if event.key == pygame.K_e:
+            #    start_experiment = False
+            #    outlet.push_sample(['end_experiment'])
 
     t = pygame.time.get_ticks() - t0
     
@@ -93,29 +114,45 @@ while running:
     for char, params in cells.items():
         j, i = params['id'] // n_cols, params['id'] % n_cols
         
-        speed = speed_func(t, params['frequency'])
         
-        if speed < params['previous_speed'] and not params['decrease']:
-            params['decrease'] = True
-            #if char == 'А' and start_experiment:
-            outlet.push_sample([f'letter_{char}_max'])
-        elif speed > params['previous_speed'] and params['decrease']:
-            params['decrease'] = False
-            params['alpha'] = random.randint(0, 1) / 2 #random.random()
-            #if char == 'А' and start_experiment:
-            outlet.push_sample([f'letter_{char}_min'])
+        if params['id'] == moving_id:
             
-        params['previous_speed'] = speed
+            try:
+                speed = speed_func(t - start_t, params['frequency'])
+            except ValueError:
+                start_t = t
+                speed = speed_func(t - start_t, params['frequency'])
+                moving_id = random.choice(range(len(alphabet)))
+            
+            dx = params['amplitude'] * speed
+            dy = 0
+            
+            
+        else:
+            dx = 0
+            dy = 0
+        
+        #if speed < params['previous_speed'] and not params['decrease']:
+        #    params['decrease'] = True
+        #    #if char == 'А' and start_experiment:
+        #    outlet.push_sample([f'letter_{char}_max'])
+        #elif speed > params['previous_speed'] and params['decrease']:
+        #    params['decrease'] = False
+        #    params['alpha'] = random.randint(0, 1) / 2 #random.random()
+        #    #if char == 'А' and start_experiment:
+        #    outlet.push_sample([f'letter_{char}_min'])
+            
+        #params['previous_speed'] = speed
         
         
-        dirx = math.cos(math.tau * params['alpha'])
-        diry = math.sin(math.tau * params['alpha'])
-        
-        dx = params['amplitude'] * dirx * speed
-        dy = params['amplitude'] * diry * speed
+        #dirx = math.cos(math.tau * params['alpha'])
+        #diry = math.sin(math.tau * params['alpha'])
+        #
+        #dx = params['amplitude'] * dirx * speed
+        #dy = params['amplitude'] * diry * speed
         
         #pygame.draw.rect(screen, (255, 0, 0), (i * w, j * h, w, h), 1)
-        screen.blit(params['shadow'], (i * w + w / 2 - params['shadow'].get_width() / 2 + dx, j * h + h / 2 - params['shadow'].get_height() / 2 - dy))
+        screen.blit(params['letter'], (i * w + w / 2 - params['letter'].get_width() / 2 + dx, j * h + h / 2 - params['letter'].get_height() / 2 - dy))
         #screen.blit(params['letter'], (i * w + w / 2 - params['letter'].get_width() / 2, j * h + h / 2 - params['letter'].get_height() / 2))
     
     
